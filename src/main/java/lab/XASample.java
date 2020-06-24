c494c66618d987513c97e5c95b18bd23520729a7
Roman Somak <r.syomak@gmail.com>
2020-06-24 20:45:31 +0300

    done 2

--- src/main/java/lab/XASample.java
+++ src/main/java/lab/XASample.java
@@ -13,10 +13,14 @@ public class XASample {
     javax.sql.XADataSource xaDS2;
     javax.sql.XAConnection xaconn1;
     javax.sql.XAConnection xaconn2;
+    javax.sql.XAConnection xaconn3;
     javax.transaction.xa.XAResource xares1;
     javax.transaction.xa.XAResource xares2;
+    javax.transaction.xa.XAResource xares3;
     java.sql.Connection conn1;
     java.sql.Connection conn2;
+    java.sql.Connection conn3;
+
 
 
     @EqualsAndHashCode
@@ -54,8 +58,11 @@ public class XASample {
         byte[] bqual = new byte[]{0x01, 0x22, 0x00};
         byte[] gtrid2 = new byte[]{0x41, 0x13, 0x55, 0x66};
         byte[] bqual2 = new byte[]{0x00, 0x23, 0x00};
+        byte[] gtrid3 = new byte[]{0x41, 0x13, 0x13, 0x66};
+        byte[] bqual3 = new byte[]{0x00, 0x13, 0x00};
         int rc1 = 0;
         int rc2 = 0;
+        int rc3 = 0;
 
         try {
             javax.naming.InitialContext context = new javax.naming.InitialContext();
@@ -70,55 +77,71 @@ public class XASample {
             hotelDataSource.setUser("postgres");
             hotelDataSource.setPassword("1111");
 
+            PGXADataSource amountDataSource = new PGXADataSource();
+            amountDataSource.setUrl("jdbc:postgresql://localhost:5432/my_db");
+            amountDataSource.setUser("postgres");
+            amountDataSource.setPassword("1111");
+
             xaconn1 = flyDataSource.getXAConnection();
             xaconn2 = hotelDataSource.getXAConnection();
+            xaconn3 = amountDataSource.getXAConnection();
+
 
             conn1 = xaconn1.getConnection();
             conn2 = xaconn2.getConnection();
+            conn3 = xaconn3.getConnection();
+
 
             xares1 = xaconn1.getXAResource();
             xares2 = xaconn2.getXAResource();
+            xares3 = xaconn3.getXAResource();
+
 
             javax.transaction.xa.Xid xid1 = this.new XID(100, gtrid, bqual);
             javax.transaction.xa.Xid xid2 = this.new XID(100, gtrid2, bqual2);
+            javax.transaction.xa.Xid xid3 = this.new XID(100, gtrid3, bqual3);
+
 
             xares1.start(xid1, javax.transaction.xa.XAResource.TMNOFLAGS);
             xares2.start(xid2, javax.transaction.xa.XAResource.TMNOFLAGS);
+            xares3.start(xid3, javax.transaction.xa.XAResource.TMNOFLAGS);
+
 
             String insertFlyQuery = "INSERT INTO fly_booking.fly_booking" +
                     "(id, client_name, fly_number, fly_from, fly_to, date) " +
-                    "VALUES (1, 'ROMAN', 'BIRD 1', 'HERE', 'THERE', '01/01/2000')";
+                    "VALUES (3, 'ROMAN', 'BIRD 1', 'HERE', 'THERE', '01/01/2000')";
 
             Statement statement1 = conn1.createStatement();
             statement1.execute(insertFlyQuery);
 
             String insertHotelQuery = "INSERT INTO hotel_booking.hotel_booking" +
                     "(id, client_name, hotel_name, arrival, departure) " +
-                    "VALUES (1, 'ROMAN', 'MIR', '01/01/2000', '07/01/2000')";
+                    "VALUES (3, 'ROMAN', 'MIR', '01/01/2000', '07/01/2000')";
 
             Statement statement2 = conn2.createStatement();
             statement2.execute(insertHotelQuery);
 
+            String reduceAmount = "UPDATE my_schema.my_table " +
+                    "SET amount = amount - 100" +
+                    "WHERE id = 111";
+
+            Statement statement3 = conn3.createStatement();
+            statement3.execute(reduceAmount);
+
             xares1.end(xid1, javax.transaction.xa.XAResource.TMSUCCESS);
             xares2.end(xid2, javax.transaction.xa.XAResource.TMSUCCESS);
+            xares3.end(xid3, javax.transaction.xa.XAResource.TMSUCCESS);
+
 
             try {
                 rc1 = xares1.prepare(xid1);
-                if (rc1 == javax.transaction.xa.XAResource.XA_OK) {
-                    rc2 = xares2.prepare(xid2);
-                    if (rc2 == javax.transaction.xa.XAResource.XA_OK) {
-                        xares1.commit(xid1, false);
-                        xares2.commit(xid2, false);
-                    } else if (rc2 == javax.transaction.xa.XAException.XA_RDONLY) {
-                        xares1.commit(xid1, false);
-                    }
-                } else if (rc1 == javax.transaction.xa.XAException.XA_RDONLY) {
-                    rc2 = xares2.prepare(xid2);
-                    if (rc2 == javax.transaction.xa.XAResource.XA_OK) {
-                        xares2.commit(xid1, false);
-                    } else if (rc2 == javax.transaction.xa.XAException.XA_RDONLY) {
-                    }
-                }
+                rc2 = xares2.prepare(xid2);
+                rc3 = xares3.prepare(xid3);
+
+                xares1.commit(xid1, false);
+                xares2.commit(xid2, false);
+                xares3.commit(xid3, false);
+
             } catch (javax.transaction.xa.XAException xae) {
                 System.out.println("Distributed transaction prepare/commit failed. " +
                         "Rolling it back.");
@@ -139,6 +162,13 @@ public class XASample {
                     System.out.println("XAException error code = " + xae2.errorCode);
                     System.out.println("XAException message = " + xae2.getMessage());
                 }
+                try {
+                    xares3.rollback(xid3);
+                } catch (javax.transaction.xa.XAException xae3) {
+                    System.out.println("distributed Transaction rollback xares3 failed");
+                    System.out.println("XAException error code = " + xae3.errorCode);
+                    System.out.println("XAException message = " + xae3.getMessage());
+                }
             }
 
             try {
@@ -153,6 +183,13 @@ public class XASample {
                 xaconn2.close();
             } catch (Exception e) {
                 System.out.println("Failed to close connection 2: " + e.toString());
+                e.printStackTrace();
+            }
+            try {
+                conn3.close();
+                xaconn3.close();
+            } catch (Exception e) {
+                System.out.println("Failed to close connection 3: " + e.toString());
                 e.printStackTrace();
             }
         } catch (java.sql.SQLException sqe) {
